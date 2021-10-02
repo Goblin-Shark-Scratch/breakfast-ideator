4cff4dd8563284d383e26a5c483bcd2e41a533c4
Reland Boyle <reland.boyle@gmail.com>
2021-10-02 17:24:20 -0400

    starting on mainController

---
+++ server/controllers/mainController.js
@@ -0,0 +1,44 @@
+const db = require('../models/UserModel');
+
+const mainController = {};
+// import API
+
+
+
+mainController.fetchIngredients = (req, res, next) => {
+
+};
+
+mainController.searchRecipe = (req, res, next) => {
+// connect to API to find recipes matching a query
+
+https://api.spoonacular.com/recipes/findByIngredients?apikey=API_KEY
+
+next();
+};
+
+
+mainController.addIngredient = (req, res, next) => {
+
+  next();
+};
+
+
+mainController.removeIngredient = (req, res, next) => {
+
+  next();
+};
+
+
+mainController.updateIngredient = (req, res, next) => {
+
+  next();
+};
+
+
+mainController.getRecents = (req, res, next) => {
+
+  next();
+};
+
+
