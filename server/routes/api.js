882729d7fe587a116e346397044ef26907846eaf
dtjones404 <dustintaylorjones@gmail.com>
2021-10-04 15:23:26 -0400

    Removed sessionRequired middlewares.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,66 +1,35 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const mainController = require('../controllers/mainController');
 const router = express.Router();
 
 // example: router.get("/page")
 
-router.get(
-  '/recipes',
-  authController.sessionRequired,
-  mainController.searchRecipe,
-  (req, res) => {
-    res.status(200).json(res.locals.recipes);
-  }
-);
+router.get('/recipes', mainController.searchRecipe, (req, res) => {
+  res.status(200).json(res.locals.recipes);
+});
 
 router.get('/recipes/:id', mainController.getMoreRecipeInfo, (req, res) => {
   res.status(200).json(res.locals.instructions);
 });
 
-router.get(
-  '/ingredients',
-  authController.sessionRequired,
-  mainController.fetchIngredients,
-  (req, res) => {
-    res.status(200).json(res.locals.ingredients);
-  }
-);
+router.get('/ingredients', mainController.fetchIngredients, (req, res) => {
+  res.status(200).json(res.locals.ingredients);
+});
 
-router.put(
-  '/ingredients',
-  authController.sessionRequired,
-  mainController.updateIngredients,
-  (req, res) => {
-    res.status(200).json(res.locals.ingredients);
-  }
-);
+router.put('/ingredients', mainController.updateIngredients, (req, res) => {
+  res.status(200).json(res.locals.ingredients);
+});
 
-router.get(
-  '/favorites',
-  authController.sessionRequired,
-  mainController.getFavorites,
-  (req, res) => {
-    res.status(200).json(res.locals.favorites);
-  }
-);
+router.get('/favorites', mainController.getFavorites, (req, res) => {
+  res.status(200).json(res.locals.favorites);
+});
 
-router.post(
-  '/favorites',
-  authController.sessionRequired,
-  mainController.addFavorite,
-  (req, res) => {
-    res.status(200).json(res.locals.favorites);
-  }
-);
+router.post('/favorites', mainController.addFavorite, (req, res) => {
+  res.status(200).json(res.locals.favorites);
+});
 
-router.delete(
-  '/favorites',
-  authController.sessionRequired,
-  mainController.removeFavorite,
-  (req, res) => {
-    res.status(200).json(res.locals.favorites);
-  }
-);
+router.delete('/favorites', mainController.removeFavorite, (req, res) => {
+  res.status(200).json(res.locals.favorites);
+});
 
 module.exports = router;
