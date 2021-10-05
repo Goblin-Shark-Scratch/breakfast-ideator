f1863e0e775572058f13fa45b81fbbbb376e14e3
kingfly555 <money@localhost.localdomain>
2021-10-05 14:53:47 -0500

    Bypassed a few API calls in mainController for testing

--- server/server.js
+++ server/server.js
@@ -40,6 +40,7 @@ app.use((req, res) =>
 
 // express error handler
 app.use((err, req, res, next) => {
+  console.log(err)
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
